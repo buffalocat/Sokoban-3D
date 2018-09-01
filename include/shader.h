aca9eef833f727611a114a9b876e0e982baedc91
chris-hunt-98 <christ-hunt-98@users.noreply.github.com>
2018-08-31 20:47:07 -0700

    Initial commit

---
+++ include/shader.h
@@ -0,0 +1,25 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class Shader
+{
+public:
+    unsigned int ID;
+
+    Shader(const GLchar* vertexPath, const GLchar* fragmentPath);
+    void use();
+    void setBool(const std::string &name, bool value) const;
+    void setInt(const std::string &name, int value) const;
+    void setFloat(const std::string &name, float value) const;
+    void setMat4(const std::string &name, const glm::mat4 &mat) const;
+};
+
+#endif // SHADER_H
---
+++ main.cpp
@@ -0,0 +1,125 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+#include "Shader.h"
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+int main(void) {
+    // Standard Init things
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, false);
+
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sokoban 3D", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    float cubeVertices[24];
+    for (int i = 0; i < 8; ++i) {
+        cubeVertices[3*i] = (i & 1) - 0.5f;
+        cubeVertices[3*i+1] = ((i >> 1) & 1) - 0.5f;
+        cubeVertices[3*i+2] = ((i >> 2) & 1) - 0.5f;
+    }
+
+    int cubeTriangles[36];
+    int i = 0;
+    for (int a : {1,2,4}) {
+        for (int b : {1,2,4}) {
+            if (a == b) continue;
+            for (int x : {0, a, a|b, 7, 7 & (~a), 7 & (~(a|b))}) {
+                cubeTriangles[i] = x;
+                ++i;
+            }
+        }
+    }
+
+    // Vertex Array Object
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    // Vertex Buffer Object
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Element Buffer Object
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cubeTriangles), cubeTriangles, GL_STATIC_DRAW);
+
+    // Load and ccompile shaders
+
+    Shader shader = Shader("shaders\\shader.vs", "shaders\\shader.fs");
+
+    glClearColor(0.2f, 0.1f, 0.3f, 1.0f);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    while(!glfwWindowShouldClose(window)) {
+        processInput(window);
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        float radius = 10.0f;
+        float camX = sin(glfwGetTime()) * radius;
+        float camZ = cos(glfwGetTime()) * radius;
+
+        glm::mat4 model = glm::mat4();
+        glm::mat4 view = glm::lookAt(glm::vec3(camX, 3.0f, camZ),
+                                     glm::vec3(0.0f, 0.0f, 0.0f),
+                                     glm::vec3(0.0f, 1.0f, 0.0f));
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCREEN_WIDTH/(float)SCREEN_HEIGHT, 0.1f, 100.0f);
+
+        // Draw the cube
+        shader.setMat4("view", view);
+        shader.setMat4("projection", projection);
+        shader.setMat4("model", model);
+        shader.use();
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    return 0;
+}
